f31f777341c933f9ffdd4ba908ca261e5487fc5b
Rafa Mardegan <rafa.mardegan@gmail.com>
2022-01-24 17:14:02 -0300

    Fix font-size

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 $(document).ready(() => {
 
+  let navbar = document.querySelector('.wz-navbar-nav');
+  for (let nav of navbar.getElementsByClassName('nav-link')) {
+    let href = nav.getAttribute('href');
+    let anchor = $(href).offset();
+    window.scrollTo(anchor);
+  }
+
   // Bootstrap Carousel
   const wzCarousel = document.querySelector('#wz-carousel');
   new bootstrap.Carousel(wzCarousel, {
